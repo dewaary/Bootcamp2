2e757eff482ed0b1acf1dc2313fee9ff3fcea02d
dewa.g.ari28 <dewa.g.ari28@gmail.com>
2021-06-07 22:26:22 +0700

    Created Find All Electronic Device

---
+++ JsonManipulation/FindAllElectronicDevice.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const data = require('./data.json');
+
+const FindAllelectronic = (findElectronic) => {
+    let dataBulk = [];
+    for(let i in data) {
+        let dataJson = data[i];
+        if(dataJson.type == "electronic") {
+            let resp = {
+                inventory:dataJson.inventory_id,
+                name:dataJson.name,
+                nameType:dataJson.type,
+                nameTags:dataJson.tags,
+                PlacementRoomId:dataJson.placement.room_id,
+                PlacementName:dataJson.placement.name,
+            }
+            dataBulk.push(resp);
+        }        
+    }
+    console.log(dataBulk);
+    return true;
+}
+
+console.log(FindAllelectronic());
+
+
+
+
+
+
+
+// const isPalindrome = (inStr) => {
+//     inStr = inStr.toLowerCase();
+//     for (let i=0; i<inStr.length; i+=1) {
+//         if(inStr[i] !== inStr[inStr.length - 1 - i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+
+// const fs = require('fs');
+
+// var rawdata = fs.readFileSync('data.json');
+
+// jsonQuery('placement[name=Meeting Room].name', {
+//     rawdata: rawdata
+    
+// })
+
+
+
+
+
